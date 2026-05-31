cf4bc5a02b3b26200280224c52c46134e808cc7d
Yang Li <34206183+yanglimath@users.noreply.github.com>
2026-05-31 11:27:11 +0800

    Add BCH code and generator polynomial to exportsUpdate src/pylinearcode/__init__.py
    Added BCH code and generator polynomial to the module exports.

--- src/pylinearcode/__init__.py
+++ src/pylinearcode/__init__.py
@@ -7,15 +7,21 @@ array backends.
 
 from .bounds import griesmer_bound_length, hamming_sphere_volume, singleton_bound
 from .constructors import (
+    bch_code,
+    bch_generator_polynomial,
     cyclic_code_from_generator,
+    extended_hamming_code,
     hamming_code,
     parity_check_code,
     random_linear_code,
+    reed_muller_code,
     reed_solomon_code,
     repetition_code,
+    simplex_code,
     whole_space_code,
     zero_code,
 )
+from .enumerators import krawtchouk, macwilliams_transform
 from .exceptions import ComputationLimitError, LinearCodeError
 from .fields import FiniteField, GF
 from .linear_code import LinearCode, SystematicForm
@@ -27,16 +33,22 @@ __all__ = [
     "LinearCode",
     "LinearCodeError",
     "SystematicForm",
+    "bch_code",
+    "bch_generator_polynomial",
     "cyclic_code_from_generator",
+    "extended_hamming_code",
     "griesmer_bound_length",
     "hamming_code",
     "hamming_sphere_volume",
+    "krawtchouk",
+    "macwilliams_transform",
     "parity_check_code",
     "random_linear_code",
+    "reed_muller_code",
     "reed_solomon_code",
     "repetition_code",
+    "simplex_code",
     "singleton_bound",
     "whole_space_code",
     "zero_code",
 ]
-
